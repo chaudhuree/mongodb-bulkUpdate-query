4513c195378dce85e61033e6752fef4b4f6cc3b6
chaudhuree <67362262+chaudhuree@users.noreply.github.com>
2023-03-30 10:23:33 +0600

    Create bulk.js

---
+++ bulk.js
@@ -0,0 +1,12 @@
+    cart is here an array of objects
+    const bulkOps = cart.map((item) => {
+      return {
+        updateOne: {
+          filter: { _id: item._id },
+          update: { $inc: { quantity: -0, sold: +1 } },
+        },
+      };
+    });
+
+    const updated = await Product.bulkWrite(bulkOps, {});
+    console.log("blk updated", updated);
